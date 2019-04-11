e6bf9a74f6b2de18df0c1a1bf851679d46fc1043
grdw <gerard@wetransfer.com>
2019-04-11 11:49:39 +0200

    Add rubocop and fix all it's warnings

--- lib/serializer.rb
+++ lib/serializer.rb
@@ -4,11 +4,11 @@ require 'serializer/group_serializer'
 class Serializer
   class SerializerError < StandardError; end
 
-  ARRAYS = %w(
+  ARRAYS = %w[
     Array
     ActiveRecord_AssociationRelation
     ActiveRecord_Associations_CollectionProxy
-  )
+  ].freeze
 
   Attribute = Struct.new(:key, :condition, :from, :serializer, :options)
 
@@ -32,24 +32,7 @@ class Serializer
     self.class.attributes.each_with_object({}) do |attribute, obj|
       next if attribute.condition && !public_send(attribute.condition)
 
-      extraction_key = attribute.from || attribute.key
-
-      # Fetches a value from an attribute by checking if there's a ..
-      # .. static value set, or a ..
-      # .. method defined in the serializer, or a ..
-      # .. method/attribute defined in the object or ..
-      # .. it raises an error
-      value = if attribute.options.has_key?(:static_value)
-                attribute.options.fetch(:static_value)
-              elsif respond_to?(extraction_key)
-                public_send(extraction_key)
-              elsif object.respond_to?(extraction_key)
-                object.public_send(extraction_key)
-              else
-                raise SerializerError, "unknown attribute '#{extraction_key}'"
-              end
-
-      obj[attribute.key] = serialize_value(value, attribute.serializer)
+      obj[attribute.key] = serialize_value(attribute)
     end
   end
 
@@ -61,13 +44,35 @@ class Serializer
 
   private
 
-  def serialize_value(value, serializer)
-    return value unless serializer
+  def serialize_value(attribute)
+    value = extract_value(attribute)
+    serializer = attribute.serializer
 
     if ARRAYS.any? { |match| value.class.to_s.end_with?(match) }
       value.map { |v| serializer.new(v).to_h }
-    else
+    elsif serializer
       serializer.new(value).to_h
+    else
+      value
+    end
+  end
+
+  # Fetches a value from an attribute by checking if there's a ..
+  # .. static value set, or a ..
+  # .. method defined in the serializer, or a ..
+  # .. method/attribute defined in the object or ..
+  # .. it raises an error
+  def extract_value(attribute)
+      extraction_key = attribute.from || attribute.key
+
+    if attribute.options.key?(:static_value)
+      attribute.options.fetch(:static_value)
+    elsif respond_to?(extraction_key)
+      public_send(extraction_key)
+    elsif object.respond_to?(extraction_key)
+      object.public_send(extraction_key)
+    else
+      raise SerializerError, "unknown attribute '#{extraction_key}'"
     end
   end
 end
--- seri.gemspec
+++ seri.gemspec
@@ -1,32 +1,35 @@
-
-lib = File.expand_path("../lib", __FILE__)
+lib = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require "serializer/version"
+require 'serializer/version'
 
 Gem::Specification.new do |spec|
-  spec.name          = "seri"
+  spec.name          = 'seri'
   spec.version       = Serializer::VERSION
-  spec.authors       = ["grdw"]
-  spec.email         = ["gerard@wetransfer.com"]
+  spec.authors       = ['grdw']
+  spec.email         = ['gerard@wetransfer.com']
 
-  spec.summary       = %q{A basic serializer}
-  spec.description   = %q{A basic serializer which can suite as a replacement to other gems}
-  spec.homepage      = "https://github.com/grdw/seri"
-  spec.license       = "MIT"
+  spec.summary       = 'A basic serializer'
+  spec.description   = 'A basic serializer'
+  spec.homepage      = 'https://github.com/grdw/seri'
+  spec.license       = 'MIT'
 
   # Specify which files should be added to the gem when it is released.
-  # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
-  spec.files         = Dir.chdir(File.expand_path('..', __FILE__)) do
-    `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
+  # The `git ls-files -z` loads the files in the RubyGem that have been added
+  # into git.
+  spec.files         = Dir.chdir(File.expand_path(__dir__)) do
+    `git ls-files -z`.split("\x0").reject do |f|
+      f.match(%r{^(test|spec|features)/})
+    end
   end
-  spec.bindir        = "exe"
+  spec.bindir        = 'exe'
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
-  spec.require_paths = ["lib"]
+  spec.require_paths = ['lib']
 
-  spec.add_dependency "oj", "~> 3.7"
-  spec.add_dependency "appsignal", "~> 2.7"
+  spec.add_dependency 'appsignal', '~> 2.7'
+  spec.add_dependency 'oj', '~> 3.7'
 
-  spec.add_development_dependency "bundler", "~> 1.16"
-  spec.add_development_dependency "rake", "~> 10.0"
-  spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency 'bundler', '~> 1.16'
+  spec.add_development_dependency 'rake', '~> 10.0'
+  spec.add_development_dependency 'rspec', '~> 3.0'
+  spec.add_development_dependency 'rubocop', '~> 0.67.2'
 end
