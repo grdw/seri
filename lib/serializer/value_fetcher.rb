944b1e54e92680b8a5e8e9a832edfbe17aa6548f
grdw <gerard@wetransfer.com>
2020-05-06 10:31:07 +0200

    Wrap every class with Serializer

--- lib/serializer/value_fetcher.rb
+++ lib/serializer/value_fetcher.rb
@@ -1,59 +1,61 @@
-class ValueFetcher
-  class SerializerError < StandardError; end
+class Serializer
+  class ValueFetcher
+    class SerializerError < StandardError; end
 
-  ARRAYS = %w[
-    Array
-    ActiveRecord_AssociationRelation
-    ActiveRecord_Associations_CollectionProxy
-  ].freeze
+    ARRAYS = %w[
+      Array
+      ActiveRecord_AssociationRelation
+      ActiveRecord_Associations_CollectionProxy
+    ].freeze
 
-  def self.fetch(attribute, object, serializer)
-    new(attribute, object, serializer).fetch
-  end
-
-  private_class_method :new
-
-  def initialize(attribute, object, serializer)
-    @attribute = attribute
-    @values = [
-      StaticValue.new(attribute),
-      SerializedValue.new(attribute, serializer),
-      HashValue.new(attribute, object),
-      ObjectValue.new(attribute, object)
-    ]
-  end
-
-  def fetch
-    serializer = @attribute.serializer
-
-    return value unless serializer
-
-    if array?
-      value.map { |item| serializer.new(item).to_h }
-    else
-      serializer.new(value).to_h
+    def self.fetch(attribute, object, serializer)
+      new(attribute, object, serializer).fetch
     end
-  end
 
-  # Fetches a value from an attribute by checking if there's a ..
-  # .. static value set, or a ..
-  # .. method defined in the serializer, or a ..
-  # .. method/attribute defined in the object or ..
-  # .. it raises an error
-  def value
-    @value ||= begin
-      extracted_value = @values.detect(&:precondition?)
+    private_class_method :new
 
-      if extracted_value.nil?
-        raise SerializerError,
-              "unknown attribute '#{@values[0].extraction_key}'"
+    def initialize(attribute, object, serializer)
+      @attribute = attribute
+      @values = [
+        StaticValue.new(attribute),
+        SerializedValue.new(attribute, serializer),
+        HashValue.new(attribute, object),
+        ObjectValue.new(attribute, object)
+      ]
+    end
+
+    def fetch
+      serializer = @attribute.serializer
+
+      return value unless serializer
+
+      if array?
+        value.map { |item| serializer.new(item).to_h }
+      else
+        serializer.new(value).to_h
       end
-
-      extracted_value.value
     end
-  end
 
-  def array?
-    ARRAYS.any? { |match| value.class.to_s.end_with?(match) }
+    # Fetches a value from an attribute by checking if there's a ..
+    # .. static value set, or a ..
+    # .. method defined in the serializer, or a ..
+    # .. method/attribute defined in the object or ..
+    # .. it raises an error
+    def value
+      @value ||= begin
+        extracted_value = @values.detect(&:precondition?)
+
+        if extracted_value.nil?
+          raise SerializerError,
+                "unknown attribute '#{@values[0].extraction_key}'"
+        end
+
+        extracted_value.value
+      end
+    end
+
+    def array?
+      ARRAYS.any? { |match| value.class.to_s.end_with?(match) }
+    end
   end
 end
